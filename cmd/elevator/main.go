e04391d358a7e055c3b2828ad94f1a0be593b554
Szymon Masternak <51755583+szymonmasternak@users.noreply.github.com>
2025-02-02 18:57:49 +0100

    Add version and help cmd line args to program

--- cmd/elevator/main.go
+++ cmd/elevator/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"github.com/szymonmasternak/TTK4145-Elevator-Project/internal/elevutils"
-	"github.com/szymonmasternak/TTK4145-Elevator-Project/internal/logger"
 	_ "github.com/szymonmasternak/TTK4145-Elevator-Project/libs/Network-go/network/bcast"
 	_ "github.com/szymonmasternak/TTK4145-Elevator-Project/libs/Network-go/network/localip"
 	_ "github.com/szymonmasternak/TTK4145-Elevator-Project/libs/Network-go/network/peers"
@@ -11,10 +10,8 @@ import (
 )
 
 func main() {
-	Log := logger.GetLogger()
 	gitHash := elevutils.GetGitHash()
-
-	Log.Info().Msgf("Git Hash: %s", gitHash)
+	elevutils.ProcessCmdArgs()
 
 	node := nodebroadcast.NewNodeBroadcast(gitHash, "127.0.0.1", 20005, 1, "master")
 	node.StartBroadcasting()
