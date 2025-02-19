39786d66964f6f080d250f5d54505156d95772c4
Szymon Masternak <51755583+szymonmasternak@users.noreply.github.com>
2025-02-19 10:32:43 +0100

    Further Refactoring

--- internal/elevstate/elevstate.go
+++ internal/elevstate/elevstate.go
@@ -21,6 +21,8 @@ type ElevatorState struct {
 	doorOpenDuration_s  time.Duration
 
 	io *elevio.ElevatorIO //internal pointer
+
+	endTime time.Time //We need to figure out if this is really needed
 }
 
 func NewElevatorState(ioDriver *elevio.ElevatorIO) *ElevatorState {
@@ -74,110 +76,104 @@ func (es *ElevatorState) Print() {
 	Log.Info().Msgf("  +--------------------+")
 }
 
-func setAllLights(es Elevator) {
+func (es *ElevatorState) setAllLights() {
 	for floor := 0; floor < N_FLOORS; floor++ {
 		for btn := 0; btn < N_BUTTONS; btn++ {
-			outputDevice.RequestButtonLight(floor, Button(btn), es.Requests[floor][btn])
+			es.io.RequestButtonLight(floor, Button(btn), es.Requests[floor][btn])
 		}
 	}
 }
 
-func Fsm_onRequestButtonPress(btn_floor int, btn_type Button) {
-	Log.Info().Msgf("\nFsm_onRequestButtonPress(%d, %s)\n", btn_floor, btn_type.ToString())
-	elevator.Print()
+func (es *ElevatorState) FsmOnRequestButtonPress(btn_floor int, btn_type Button) {
+	Log.Info().Msgf("Fsm_onRequestButtonPress(%d, %s)\n", btn_floor, btn_type.ToString())
 
-	switch elevator.Behaviour {
+	switch es.Behaviour {
 	case EB_DoorOpen:
-		if Requests_shouldClearImmediately(elevator, btn_floor, btn_type) {
-			Timer_start(elevator.doorOpenDuration_s)
+		if es.RequestsShouldClearImmediately(btn_floor, btn_type) {
+			es.endTime = time.Now().Add(es.doorOpenDuration_s)
 		} else {
-			elevator.Requests[btn_floor][btn_type] = 1
+			es.Requests[btn_floor][btn_type] = 1
 		}
 
 	case EB_Moving:
-		elevator.Requests[btn_floor][btn_type] = 1
+		es.Requests[btn_floor][btn_type] = 1
 
 	case EB_Idle:
-		elevator.Requests[btn_floor][btn_type] = 1
-		pair := Requests_chooseDirection(elevator)
-		elevator.Dirn = pair.Dirn
-		elevator.Behaviour = pair.Behaviour
+		es.Requests[btn_floor][btn_type] = 1
+		pair := es.RequestsChooseDirection()
+		es.Dirn = pair.Dirn
+		es.Behaviour = pair.Behaviour
 
 		switch pair.Behaviour {
 		case EB_DoorOpen:
-			outputDevice.DoorLight(1)
-			Timer_start(elevator.doorOpenDuration_s)
-			elevator = Requests_clearAtCurrentFloor(elevator)
+			es.io.DoorLight(1)
+			es.endTime = time.Now().Add(es.doorOpenDuration_s)
+			es.RequestsClearAtCurrentFloor()
 
 		case EB_Moving:
-			outputDevice.MotorDirection(elevator.Dirn)
+			es.io.MotorDirection(es.Dirn)
 
 		case EB_Idle:
 			// Do nothing
 		}
 	}
 
-	setAllLights(elevator)
+	es.setAllLights()
 
-	Log.Info().Msgf("\nNew state:")
-	elevator.Print()
+	Log.Info().Msgf("New state:")
 }
 
 // Handles elevator arrival at a new floor
-func Fsm_onFloorArrival(newFloor int) {
-	Log.Info().Msgf("\nFsm_onFloorArrival(%d)\n", newFloor)
-	elevator.Print()
+func (es *ElevatorState) FsmOnFloorArrival(newFloor int) {
+	Log.Info().Msgf("Fsm_onFloorArrival(%d)\n", newFloor)
 
-	elevator.Floor = newFloor
-	outputDevice.FloorIndicator(elevator.Floor)
+	es.Floor = newFloor
+	es.io.FloorIndicator(es.Floor)
 
-	switch elevator.Behaviour {
+	switch es.Behaviour {
 	case EB_Moving:
-		if Requests_shouldStop(elevator) {
-			outputDevice.MotorDirection(D_Stop)
-			outputDevice.DoorLight(1)
-			elevator = Requests_clearAtCurrentFloor(elevator)
-			Timer_start(elevator.doorOpenDuration_s)
-			setAllLights(elevator)
-			elevator.Behaviour = EB_DoorOpen
+		if es.RequestsShouldStop() {
+			es.io.MotorDirection(D_Stop)
+			es.io.DoorLight(1)
+			//elevator = es.RequestsClearAtCurrentFloor(elevator)
+			es.RequestsClearAtCurrentFloor()
+			// Timer_start(elevator.doorOpenDuration_s)
+			es.endTime = time.Now().Add(es.doorOpenDuration_s)
+			es.setAllLights()
+			es.Behaviour = EB_DoorOpen
 		}
 	}
-
-	Log.Info().Msgf("\nNew state:")
-	elevator.Print()
 }
 
 // Handles door timeout event
-func Fsm_onDoorTimeout() {
-	Log.Info().Msgf("\nFsm_onDoorTimeout() - Closing Door")
-	elevator.Print()
+func (es *ElevatorState) FsmOnDoorTimeout() {
+	Log.Info().Msg("Closing Door")
 
-	if elevator.Behaviour == EB_DoorOpen {
+	if es.Behaviour == EB_DoorOpen {
 		// Turn off door light (Close door)
-		outputDevice.DoorLight(0)
+		es.io.DoorLight(0)
 
 		// Choose next action
-		pair := Requests_chooseDirection(elevator)
-		elevator.Dirn = pair.Dirn
-		elevator.Behaviour = pair.Behaviour
+		pair := es.RequestsChooseDirection()
+		es.Dirn = pair.Dirn
+		es.Behaviour = pair.Behaviour
 
-		switch elevator.Behaviour {
+		switch es.Behaviour {
 		case EB_DoorOpen:
 			// Restart timer if the elevator still has a request at this floor
-			outputDevice.DoorLight(1)
-			Timer_start(elevator.doorOpenDuration_s)
-			elevator = Requests_clearAtCurrentFloor(elevator)
-			setAllLights(elevator)
+			es.io.DoorLight(1)
+			es.endTime = time.Now().Add(es.doorOpenDuration_s)
+			// elevator = Requests_clearAtCurrentFloor(elevator)
+			es.RequestsClearAtCurrentFloor()
+			es.setAllLights()
 
 		case EB_Moving:
 			// Move the elevator in the chosen direction
-			outputDevice.MotorDirection(elevator.Dirn)
+			es.io.MotorDirection(es.Dirn)
 
 		case EB_Idle:
 			// Do nothing, elevator stays idle
 		}
 	}
-
-	Log.Info().Msgf("\nNew state after door timeout:")
-	elevator.Print()
+	Log.Info().Msg("New state after door timeout")
 }
